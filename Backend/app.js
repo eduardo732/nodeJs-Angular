409ff9b3559cbf6cf206ed8c337a6270289866ba
eduardo fuentes <eduardo.fuentesreyes@gmail.com>
2021-01-31 11:37:32 -0300

    Backend terminado

--- Backend/app.js
+++ Backend/app.js
@@ -13,6 +13,16 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 //CORS
+//Para las peticiones ajax de angular u jquery, con esta configuracion, funciona correctamente
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 //RUTAS
 app.use('/api', projectRoutes);
