ef606a70bbcc0120747e88490c123e16ce57ec29
eduardo fuentes <eduardo.fuentesreyes@gmail.com>
2021-02-01 20:14:59 -0300

    aboutme and create project

--- Frontend/src/app/app.routing.ts
+++ Frontend/src/app/app.routing.ts
@@ -19,4 +19,4 @@ const appRoutes: Routes = [
 //La url ** sirve para el 404, se puede crear un componente personalizado para demostrar ese error.
 
 export const appRoutingProviders: any[]= [];
-export const routing: ModuleWithProviders<any>= RouterModule.forRoot(appRoutes);
+export const routing: ModuleWithProviders<any>= RouterModule.forRoot(appRoutes, { relativeLinkResolution: 'legacy' });
--- Frontend/src/app/components/create-project/create-project.component.ts
+++ Frontend/src/app/components/create-project/create-project.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import {Project} from '../../models/project';
+import {ProjectService} from '../../services/project.service';
+import {UploadService} from '../../services/upload.service';
+import {Global} from '../../services/global';
 
 @Component({
   selector: 'app-create-project',
   templateUrl: './create-project.component.html',
-  styleUrls: ['./create-project.component.css']
+  styleUrls: ['./create-project.component.css'],
+  providers: [ProjectService, UploadService]
 })
 export class CreateProjectComponent implements OnInit {
 
-  constructor() { }
+  public title: string;
+  public project: Project;
+  public status: string;
+  public filesToUpload: Array<File>;
+
+  constructor(
+    private _projectService: ProjectService,
+    private _uploadService: UploadService
+  ) { 
+    this.title = 'Crear proyecto';
+    this.project= new Project('', '', '', '', 2021, '', '');
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form){
+    this._projectService.saveProject(this.project).subscribe(
+      response =>{
+        if (response){
+          
+          //Subir imagen
+          this._uploadService.makeFileRequest(Global.url+"uploadImage/"+response.project._id, [], this.filesToUpload, 'null')
+          .then((result:any)=>{
+            this.status='success';
+            form.reset();
+          });
+          
+        }else{
+          this.status='failed';
+        }
+      },
+      error =>{
+        console.log(<any>error);
+      }
+    );
+  }
+  fileChangeEvent(fileInput: any){
+    this.filesToUpload= <Array<File>>fileInput.target.files;
   }
 
 }
